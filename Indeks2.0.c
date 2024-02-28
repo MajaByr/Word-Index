5bdabac1740d3735daed19e856527fb1faf7e6e1
MajaByr <147385123+MajaByr@users.noreply.github.com>
2024-02-28 12:19:41 +0100

    Update and rename Indeks2.0.c

--- Indeks2.0.c
+++ Indeks2.0.c
@@ -1,7 +1,3 @@
-// Indeks2.0.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-
 #include "BSTree.h"
 #include <stdio.h>
 #include <malloc.h>
@@ -157,14 +153,3 @@ void reallocWord( char** pWord, int currsize )
 
 	memset( *pWord+currsize, 0, WORDSIZE*sizeof(char) );
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
